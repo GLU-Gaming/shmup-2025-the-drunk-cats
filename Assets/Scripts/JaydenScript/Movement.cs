075743f57a84f013bbfec82dbc6c930d8fed49ea
Jayden Gallas <159015439+JaydenGallas@users.noreply.github.com>
2025-03-18 14:30:55 +0100

    basic movement added
    ik moet op een branch dus ff commit en push

--- Assets/Scripts/JaydenScript/Movement.cs
+++ Assets/Scripts/JaydenScript/Movement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public GameObject player;
-    public Speed =
+    public float speed = 5f;
+    private float moveY;
+    private float moveX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,15 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveX = 0f;
+        moveY = 0f;
+
+        if (Input.GetKey(KeyCode.W)) moveY += 1;
+        if (Input.GetKey(KeyCode.S)) moveY -= 1;
+        if (Input.GetKey(KeyCode.A)) moveX -= 1;
+        if (Input.GetKey(KeyCode.D)) moveX += 1;
+
+        Vector3 moveDirection = new Vector3(moveX, moveY, 0).normalized;
+        transform.position += moveDirection * speed * Time.deltaTime;
     }
 }
