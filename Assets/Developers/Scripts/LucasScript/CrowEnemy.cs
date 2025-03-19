64461d430d69e38f2c0a28d1e9eded001e34f5f3
raav <127980618+raav11@users.noreply.github.com>
2025-03-19 09:31:54 +0100

    crow gaming

---
+++ Assets/Developers/Scripts/LucasScript/CrowEnemy.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CrowEnemy : MonoBehaviour
+{
+
+    private float speedCrow;
+
+    [SerializeField] Rigidbody rbCrow;
+
+    [SerializeField] Transform tfCrow;
+
+    private float healthCrow = 3f;
+    void Start()
+    {
+
+        MoveCrow();
+        
+    }
+
+
+    void Update()
+    {
+
+        CheckHealth();
+        
+    }
+
+    private void MoveCrow()
+    {
+
+        speedCrow = -10f;
+
+        rbCrow.linearVelocity = transform.right * speedCrow;
+
+        tfCrow.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+    }
+
+    private void CheckHealth()
+    {
+
+        if (healthCrow <= 0)
+        {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+}
