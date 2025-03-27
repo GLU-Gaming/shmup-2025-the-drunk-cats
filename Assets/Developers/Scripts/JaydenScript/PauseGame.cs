dc63606899065ad292e7e8676223332f37651a13
Jayden Gallas <159015439+JaydenGallas@users.noreply.github.com>
2025-03-27 10:50:48 +0100

    fiks UI main scene

--- Assets/Developers/Scripts/JaydenScript/PauseGame.cs
+++ Assets/Developers/Scripts/JaydenScript/PauseGame.cs
@@ -70,6 +70,7 @@ public class PauseGame : MonoBehaviour
     }
     public void BackToPause()
     {
+        quitMenuUI.SetActive(false);
         retryMenuUI.SetActive(false);
         pauseMenuUI.SetActive(true);
     }
