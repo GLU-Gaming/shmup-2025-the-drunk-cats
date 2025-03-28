6e7ee6ca34706c1fe3d9a9286dc3a4e7319c13a8
raav <127980618+raav11@users.noreply.github.com>
2025-03-28 12:16:27 +0100

    First attack done i hope so :D

--- Assets/Developers/Scripts/LucasScript/WhaleBoss.cs
+++ Assets/Developers/Scripts/LucasScript/WhaleBoss.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class WhaleBoss : MonoBehaviour
@@ -30,30 +31,50 @@ public class WhaleBoss : MonoBehaviour
 
     void Update()
     {
+     CannonShoot();
+
+    }
+
+    private void CannonShoot()
+    {
         laserTCD += Time.deltaTime;
         laserTCDW += Time.deltaTime;
 
-        if (laserTCDW >= 0.3f && laserAW <= 9)
+        if (laserAW >= 6 && laserA >= 6)
+        {
+            laserTCD = 0;
+            laserTCDW = 0;
+            laserA = 0;
+            laserAW = 0;
+            laserPointWarn.transform.rotation = Quaternion.Euler(new Vector3(laserPointWarn.transform.rotation.x, laserPointWarn.transform.rotation.y, laserPZW = 0f));
+            laserPoint.transform.rotation = Quaternion.Euler(new Vector3(laserPoint.transform.rotation.x, laserPoint.transform.rotation.y, laserPZ = 0f));
+        }
+
+        if (laserTCDW >= 0.2f && laserAW <= 5)
         {
             GameObject warn = Instantiate(laserWarn, laserPointWarn.position, laserPointWarn.rotation);
             warningLasers[laserAW] = warn;
-            laserPointWarn.transform.rotation = Quaternion.Euler(new Vector3(laserPointWarn.transform.rotation.x, laserPointWarn.transform.rotation.y, laserPZW -= 10f));
+            laserPointWarn.transform.rotation = Quaternion.Euler(new Vector3(laserPointWarn.transform.rotation.x, laserPointWarn.transform.rotation.y, laserPZW -= 15f));
             laserAW++;
             laserTCDW = 0f;
         }
 
-        if (laserTCD >= 0.4f && laserA <= 9 && laserAW >= 4)
+        if (laserTCD >= 0.4f && laserA <= 5 && laserAW >= 4)
         {
             Instantiate(laser, laserPoint.position, laserPoint.rotation);
-            laserPoint.transform.rotation = Quaternion.Euler(new Vector3(laserPoint.transform.rotation.x, laserPoint.transform.rotation.y, laserPZ -= 10f));
+            laserPoint.transform.rotation = Quaternion.Euler(new Vector3(laserPoint.transform.rotation.x, laserPoint.transform.rotation.y, laserPZ -= 15f));
 
-            if (warningLasers[laserA] != null) 
+            if (warningLasers[laserA] != null)
             {
-                Destroy(warningLasers[laserA]); 
+                Destroy(warningLasers[laserA]);
             }
 
             laserA++;
             laserTCD = 0f;
         }
+
+        
     }
 }
+
+
